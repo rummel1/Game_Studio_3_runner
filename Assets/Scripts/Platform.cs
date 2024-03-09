542755de63ebbb4a1b6c41a9ab7523a0939b1008
rummel1 <erencoker@hotmail.com.tr>
2024-03-09 16:09:51 +0300

    platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformMovement : MonoBehaviour
+public class Platform : MonoBehaviour
 {
     // Start is called before the first frame update
+    public MeshRenderer MeshRenderer;
+    public static Vector3 endPoint;
     void Start()
     {
-        
+        endPoint = MeshRenderer.bounds.size + this.transform.position;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    [SerializeField] private GameObject platform1;
+    [SerializeField] private GameObject platform2;
+    [SerializeField] private Transform player;
+    
+
+    private float secondTime=60.1f;
+    private float firstTime = 90.1f;
+    
+    void Start()
+    {
+        InvokeRepeating(nameof(FirstPlatformSpawn),0,6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > secondTime)
+        {
+            CancelInvoke(nameof(FirstPlatformSpawn));
+            Invoke(nameof(SecondPlatform),0);
+            secondTime += 90.1f;
+            return;
+        }
+
+        if (Time.time>firstTime)
+        {
+            CancelInvoke(nameof(SecondPlatformSpawn));
+            Invoke(nameof(FirstPlatform),0);
+            firstTime += 90.1f;
+            return;
+        }
+    }
+
+    
+    private void SecondPlatformSpawn()
+    {
+       
+        GameObject nesne = Instantiate(platform2,new Vector3(platform2.transform.position.x,platform2.transform.position.y,Platform.endPoint.z), Quaternion.identity);
+        Platform platform = nesne.GetComponent<Platform>();
+        Destroy(nesne,25);
+    }
+    
+    private void FirstPlatformSpawn()
+    {
+        GameObject nesne = Instantiate(platform1,new Vector3(platform1.transform.position.x,platform1.transform.position.y,Platform.endPoint.z), Quaternion.identity);
+        Destroy(nesne,25);
+    }
+
+    private void SecondPlatform()
+    {
+        InvokeRepeating(nameof(SecondPlatformSpawn), 0f, 10);
+    }
+    private void FirstPlatform()
+    {
+        InvokeRepeating(nameof(FirstPlatformSpawn), 0f, 10);
+    }
+}
