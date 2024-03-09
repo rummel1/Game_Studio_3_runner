a353a3e5a297170aeab952a0f54425ff905e5540
rummel1 <erencoker@hotmail.com.tr>
2024-03-09 12:56:38 +0300

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [System.Serializable]
 public class Boundary
 {
-    public float minZ, maxZ;
+    public float minX, maxX;
 }
 public class PlayerController: MonoBehaviour
 {
@@ -18,6 +18,7 @@ public class PlayerController: MonoBehaviour
    
     [SerializeField] private float _velocityRotationX;
     [SerializeField] private float _velocityRotationZ;
+    [SerializeField] private float _speedBoat;
     
     
      public static float _forwardSpeed=150;
@@ -32,12 +33,18 @@ public class PlayerController: MonoBehaviour
     void FixedUpdate()
     {
         _movement();
+        ForwardMovement();
     }
 
+    private void ForwardMovement()
+    {
+        _rb.velocity = new Vector3(_rb.velocity.x, _rb.velocity.y, _forwardSpeed*Time.deltaTime);
+    }
     private void _movement()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(transform.position.z,boundary.minZ,boundary.maxZ ));
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x,boundary.minX,boundary.maxX ), transform.position.y, transform.position.z);
         _rb.velocity=new Vector3(_joystick.Horizontal*_leftrightSpeed* Time.deltaTime, _rb.velocity.y,_rb.velocity.z);
+        
         
         _rb.rotation=Quaternion.Euler(_rb.velocity.z*_velocityRotationZ,0,_rb.velocity.x*-_velocityRotationX);
     }
