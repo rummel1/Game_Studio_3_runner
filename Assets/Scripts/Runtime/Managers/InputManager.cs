6b046912c082fda42fe5ce4ebca58ea9b6ab9c1a
EREN\erenc <erencoker@hotmail.com.tr>
2024-06-11 14:42:17 +0300

    fix

---
+++ Assets/Scripts/Runtime/Managers/InputManager.cs
@@ -0,0 +1,18 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Private Veriables
+
+        private float2 _inputvalues;
+
+        #endregion
+
+        #endregion
+    }
+}
